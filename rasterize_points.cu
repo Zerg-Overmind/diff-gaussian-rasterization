d56d337eee574539a1a51e377f369ba860e406fa
gqk <563041763@qq.com>
2024-03-25 19:20:16 -0700

    First commit

--- rasterize_points.cu
+++ rasterize_points.cu
@@ -61,19 +61,19 @@ RasterizeGaussiansCUDA(
   const int P = means3D.size(0);
   const int H = image_height;
   const int W = image_width;
-
+  const int K = 20;
   auto int_opts = means3D.options().dtype(torch::kInt32);
   auto float_opts = means3D.options().dtype(torch::kFloat32);
-
+  
   torch::Tensor out_color = torch::full({NUM_CHANNELS, H, W}, 0.0, float_opts);
   torch::Tensor out_depth = torch::full({1, H, W}, 0.0, float_opts);
   torch::Tensor out_alpha = torch::full({1, H, W}, 0.0, float_opts);
   torch::Tensor radii = torch::full({P}, 0, means3D.options().dtype(torch::kInt32));
   torch::Tensor proj_2D = torch::full({P, 2}, 0.0, float_opts);
   torch::Tensor conic_2D = torch::full({P, 3}, 0.0, float_opts);
-  torch::Tensor gs_per_pixel = torch::full({20, H, W}, -1.0, float_opts);
-  torch::Tensor weight_per_gs_pixel = torch::full({20, H, W}, 0.0, float_opts);
-  torch::Tensor x_mu = torch::full({20, 2, H, W}, 0.0, float_opts);
+  torch::Tensor gs_per_pixel = torch::full({K, H, W}, -1.0, float_opts);
+  torch::Tensor weight_per_gs_pixel = torch::full({K, H, W}, 0.0, float_opts);
+  torch::Tensor x_mu = torch::full({K, 2, H, W}, 0.0, float_opts);
   
 
   
