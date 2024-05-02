ade8f028d7895e37d337e5313bc0952cc2c331dd
gqk <563041763@qq.com>
2024-05-01 21:04:13 -0700

    bugs fixing

--- cuda_rasterizer/forward.cu
+++ cuda_rasterizer/forward.cu
@@ -387,7 +387,7 @@ renderCUDA(
 			{
                 gs_per_pixel[calc * H * W + pix_id] = collected_id[j];
 				weight_per_gs_pixel[calc * H * W + pix_id] = alpha * T;
-				x_mu[calc * H * W + pix_id] = d.x;
+				x_mu[calc *2 * H * W + pix_id] = d.x;
 				x_mu[(calc * 2 + 1) * H * W + pix_id] = d.y;
 
 			}
